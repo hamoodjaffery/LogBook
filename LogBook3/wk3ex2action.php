6d75edcee40f3414df74f6f2f4b4e7dc84b4f3d8
Hamood Ur Rab Jaffery <72038627+hamoodjaffery@users.noreply.github.com>
2022-04-05 16:50:54 +0100

    changed the code to enter gender
    changed the code to enter gender

--- LogBook3/wk3ex2action.php
+++ LogBook3/wk3ex2action.php
@@ -1,33 +1,23 @@
 <?php
-
-  //Age
-  //Even with the input checks the user can still enter text instead of a number.
-  //To fix this issue the html input type has to be changed.
-  if ($_POST["txtage"] < 21 && $_POST["txtage"] >= 0)
+  if ($_POST["txtage"] < 21) 
   {
-	   echo "You are under 21 years old<br/>";
-  }
-  elseif($_POST["txtage"] >= 21 && $_POST["txtage"] < 150)
-  {
-	   echo "You are  21 years old or over<br/>";
-  }
-  else{
-    echo "Enter valid age";
-  }
-
-  echo "<br/>";
-
-  //Gender
-  if($_POST["txtgender"] == "male")
-  {
-    echo "You are a man";
-  }
-  elseif($_POST["txtgender"] == "female")
-  {
-    echo "You are a woman";
+	echo "You are under 21 years old<br/>";
   }
   else
   {
-    echo "Enter your gender as specified!";
+	echo "You are  21 years old or over<br/>";
+  }
+
+  if ($_POST["txtgender"] == "MALE" || $_POST["txtgender"] == "male" || $_POST["txtgender"] == "Male")
+  {
+    echo "You are a Male";
+  }
+  elseif ($_POST["txtgender"] == "FEMALE" || $_POST["txtgender"] == "female" || $_POST["txtgender"] == "Female")
+  {
+    echo "You are a Female";
+  }
+  else 
+  {
+    echo "You haven't added a gender";
   }
 ?>
