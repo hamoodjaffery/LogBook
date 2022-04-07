6cc4b91a6296a2d1830d1dfaa37cb34c54dec1f6
Hamood Ur Rab Jaffery <72038627+hamoodjaffery@users.noreply.github.com>
2022-04-07 03:12:40 +0100

    set cookie
    completed wk6ex2action

--- LogBook6/wk6ex2action.php
+++ LogBook6/wk6ex2action.php
@@ -1,33 +1,42 @@
-<?php	
+<?php
 
-// Connect to server and select database
-$mysqli = new mysqli("localhost","root","12345","test");
-// Check connection
-if ($mysqli -> connect_errno) 
-{
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-	
-	$sql = "SELECT * from test where name = '$_GET[id]' ";
-	// Execute query
-	$row = mysqli_fetch_assoc($result);
+ $id = $_GET['id'];
+
+ setcookie('id', $id); 
+
+    // Connect to server and select database
+
+	$mysqli = new mysqli("localhost","root","12345","test");
+
+    if(mysqli_connect_errno())
+  {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }   
+
+  $sql = "SELECT * from test";
+
+    // Execute query
+    $result = mysqli_query($mysqli, $sql); 
+
+    $row = mysqli_fetch_assoc($result);
 ?>
-
 <html>
 <body>
-<form action="wk6ex2action.php" method="post">
+<form action= "wk6ex2action.php" method ="post">
 
-	Name :
-	<input type=text name=txtname value="<?php echo $row["name"] ?>" />
-	</br>
-	Phone number :
-	<input type=text name=txttelno value="<?php echo $row["phone_number"] ?>" />
-	</br>
-	Email :
-	<input type=text name=txtemail value="<?php echo $row["email"] ?>" />
-	</br>
-	<input type=submit name=btnsubmit value="save"/>
+    Name :
+    <input type=text name=txtname value="<?php echo $row['name'] ?>" readonly />
+    </br>
+
+    Phone number :
+    <input type=text name=txttelno value="<?php echo $row['phone_number'] ?>" />
+    </br>
+
+    Email :
+    <input type=text name=txtemail value="<?php echo $row['email'] ?>" />
+    </br>
+    <input type=submit name=btnsubmit value="save"/>
+
 </form>
 </body>
 </html>
